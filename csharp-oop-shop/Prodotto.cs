08a7a21052e955097876be092e24cb0f9644a558
Hadson Roque <nineonezero.light@gmail.com>
2023-04-07 17:05:27 +0200

    Create methods for Price with IVA

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -13,33 +13,48 @@ namespace csharp_oop_shop
         private string nome;
         private string descrizione;
         private decimal prezzo;
-        private int iva;
+        private decimal iva;
 
         //Costruttore del prodotto
-        public Prodotto(int codice, string nome, string descrizione, decimal prezzo, int iva)
+        public Prodotto(string nome, string descrizione, decimal prezzo, decimal iva)
         {
             //vado a dichiarare che al codice verra' generato un codice random
-            this.codice = GeneraCodice();
-            this.codice = codice;
-            this.nome = nome;
-            this.descrizione = descrizione;
-            this.prezzo = prezzo;
-            this.iva = iva;
+            codice = GetNumRam();
+
+            //richiamo i GETSET mettendoli al posto di this.
+            Nome = nome;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+            Iva = iva;
+
         }
 
-        //funzione che generera' un codice random
-        private int GeneraCodice()
+        //Metodo per generare numeri random
+        public int GetNumRam()
         {
             Random rnd = new Random();
-            return rnd.Next(50,999);
+            return rnd.Next(50, 999);
         }
 
+        //Metodo per generare Prezzo con Iva
+        public decimal PrezzoIva()
+        {
+            decimal PrezzoIva = Prezzo += Prezzo * Iva / 100;
+            return PrezzoIva;
+        }
+
+
+
         //GET & SET
+        public int Codice { get { return codice; } }
         public string Nome { get { return nome; } set { nome = value; } }
         public string Descrizione { get {  return descrizione; } set {  descrizione = value; } }
         public decimal Prezzo { get { return prezzo;  } set { prezzo = value; } }
-        public int Iva { get {  return iva; } set {  iva = value; } }
-       
+        public decimal Iva { get {  return iva; } set {  iva = value; } }
+
+        
+
+
     }
 }
  
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace csharp_oop_shop
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_oop_shop
 {   
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            Prodotto prod1 = new Prodotto("babbuino", "Stupendo bellissimo imparagonabile", 10, 22);
+            Console.WriteLine(
+                $"Il codice del tuo prodotto: {prod1.Codice} \n " +
+                $"Il nome del tuo prodotto: {prod1.Nome} \n " +
+                $"La Descrizione del tuo prodotto: {prod1.Descrizione} \n " +
+                $"Il prezzo senza iva: ${prod1.Prezzo} \n " +
+                $"L'iva del prodotto: {prod1.Iva}% \n " +
+                $"Il prezzo con l'iva: ${prod1.PrezzoIva()}"
+                );
+
+
+
         }
     }
 }
